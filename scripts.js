ae91a182e16befc102ecdfcdf91b52f71f041878
Wither19 <146484561+Wither19@users.noreply.github.com>
2024-10-10 20:23:04 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,129 @@
+const { useState, useEffect } = React;
+const app = document.querySelector("#root");
+
+function CoolHeading(props) {
+	const bigAndBold = "display-1";
+	const smol = "display-3";
+
+	const [headingSize, setHeading] = useState(false);
+
+	const headingStyle = {
+		flex: ["d-flex flex-row justify-content-center transition-header", "d-flex flex-row justify-content-center"],
+		blurb: ["blurb transition-header", "blurb"],
+	};
+
+	return (
+		<div
+			onClick={() => {
+				setHeading((headingSize) => !headingSize);
+			}}
+			className="non-select">
+			<div className={props.smooth ? headingStyle.flex[0] : headingStyle.flex[1]}>
+				<div className={headingSize ? bigAndBold : smol}>
+					{props.text ? props.text : "Welcome to the landing page"}
+				</div>
+			</div>
+			<br />
+			<div
+				className={props.smooth ? headingStyle.blurb[0] : headingStyle.blurb[1]}
+				style={{ opacity: headingSize ? "1" : "0" }}>
+				{React.createElement(
+					props.blurbTag ? props.blurbTag : "p",
+					null,
+					props.blurb ? props.blurb : "How tough are ya"
+				)}
+			</div>
+		</div>
+	);
+}
+
+function SiteList() {
+	return (
+		<>
+			<h1>Site List</h1>
+			<div className="d-flex flex-row justify-content-center flex-wrap site-list-wrapper">
+				{sites.map((site, i) => (
+					<a className="site-link" href={site.url}>
+						<div style={{ textAlign: "center" }}>
+							{site.siteName}
+							<br />
+							<span className="desc-text">{site.desc}</span>
+						</div>
+					</a>
+				))}
+			</div>
+		</>
+	);
+}
+
+// Add Random Bible Verse using API that has a link to BibleGateway!
+
+function BibleApiRef() {
+	let [fetchedVerse, setFetchedVerse] = useState({});
+	let [selectedVerse, setSelectedVerse] = useState("John 1:1");
+
+	function grabVerse(event) {
+		const verseInput = document.querySelector("input#bible-verse-selection");
+		if (event.which == 13 || event.target.id == "verse-select") {
+			fetch(`https://bible-api.com/${verseInput.value}`)
+				.then((response) => response.json())
+				.then((data) => {
+					setFetchedVerse(data);
+				});
+			verseInput.value = "";
+		}
+	}
+
+	useEffect(() => {
+		fetch(`https://bible-api.com/${selectedVerse}`)
+			.then((response) => response.json())
+			.then((data) => {
+				setFetchedVerse(data);
+			});
+	}, []);
+
+	return (
+		<>
+			<div style={{ textAlign: "center" }}>
+				<h1>Bible Verse Reference</h1>
+				<sub>
+					Powered by Tim Morgan's <a href="https://bible-api.com">Bible API</a>
+				</sub>
+				<h2 className="move-up">{fetchedVerse.reference}</h2>
+				<div className="bible-verse">{fetchedVerse.text}</div>
+				<a target="_blank" href={`https://biblegateway.com/passage/?search=${fetchedVerse.reference}`}>
+					{fetchedVerse.reference} on BibleGateway
+				</a>
+			</div>
+			<div className="d-flex flex-row justify-content-center">
+				<input id="bible-verse-selection" type="text" onKeyDown={grabVerse} />
+			</div>
+			<div className="d-flex flex-row justify-content-center">
+				<button id="verse-select" className="bible-verse-select-button" onClick={grabVerse}>
+					Go to Verse
+				</button>
+			</div>
+		</>
+	);
+}
+function WholeBunchaLineBreaks(props) {
+	const lineBreaks = [];
+	for (let b = 0; b < props.breaks; b++) {
+		lineBreaks.push(<br key={b} />);
+	}
+
+	return <>{lineBreaks}</>;
+}
+
+function MeApp() {
+	return (
+		<>
+			<CoolHeading smooth={true} />
+			<SiteList />
+			<WholeBunchaLineBreaks breaks={4} />
+			<BibleApiRef />
+		</>
+	);
+}
+
+ReactDOM.render(<MeApp />, app);
---
+++ sites.js
@@ -0,0 +1,37 @@
+const sites = [
+	{
+		siteName: "Old Landing Page",
+		desc: "Haven't worked on this for nearly a year",
+		url: "https://wither19.github.io/landing",
+	},
+	{
+		siteName: "Pokedex Project",
+		desc: "Uses PokeAPI",
+		url: "https://wither19.github.io/pokedex",
+	},
+	{
+		siteName: "That Div is a spy!",
+		desc: "A TF2 Info website I'm looking to give a facelift",
+		url: "https://wither19.github.io/thatdivisaspy",
+	},
+	{
+		siteName: "Prodigal Son Parable Project",
+		desc: "School Project made before knowing any JS at all",
+		url: "https://wither19.github.io/parable",
+	},
+	{
+		siteName: "Sandvich Clicker",
+		desc: "Idle game experiment perfectly representative of my TF2 Obsession",
+		url: "https://wither19.github.io/sandvichclicker",
+	},
+	{
+		siteName: "Vue Playground",
+		desc: "Fiddling around with Vue",
+		url: "https://wither19.github.io/vue",
+	},
+	{
+		siteName: "DEMOMAN",
+		desc: "Bottom Text",
+		url: "https://wither19.github.io/landing/laxjsdemoman",
+	},
+];
