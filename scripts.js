eee7f314368015999678e66347a7d38917ec84fb
Wither19 <146484561+Wither19@users.noreply.github.com>
2024-10-12 13:35:01 -0400

    c

--- scripts.js
+++ scripts.js
@@ -8,10 +8,7 @@ function CoolHeading(props) {
 	const [headingSize, setHeading] = useState(false);
 
 	const headingStyle = {
-		flex: [
-			"d-flex flex-row justify-content-center transition-header",
-			"d-flex flex-row justify-content-center",
-		],
+		flex: ["d-flex flex-row justify-content-center transition-header", "d-flex flex-row justify-content-center"],
 		blurb: ["blurb transition-header", "blurb"],
 	};
 
@@ -25,16 +22,11 @@ function CoolHeading(props) {
 				setHeading((headingSize) => !headingSize);
 			}}
 			className="non-select">
-			<div
-				className={props.smooth ? headingStyle.flex[0] : headingStyle.flex[1]}>
-				<div className={headingSize ? bigAndBold : smol}>
-					{props.text ? props.text : "Welcome to the landing page"}
-				</div>
+			<div className={props.smooth ? headingStyle.flex[0] : headingStyle.flex[1]}>
+				<div className={headingSize ? bigAndBold : smol}>{props.text ? props.text : "Welcome to the landing page"}</div>
 			</div>
 			<br />
-			<div
-				className={props.smooth ? headingStyle.blurb[0] : headingStyle.blurb[1]}
-				style={{ opacity: headingSize ? "1" : "0" }}>
+			<div className={props.smooth ? headingStyle.blurb[0] : headingStyle.blurb[1]} style={{ opacity: headingSize ? "1" : "0" }}>
 				<BlurbTag>{blurbText}</BlurbTag>
 			</div>
 		</div>
@@ -63,7 +55,7 @@ function SiteList() {
 // Add Random Bible Verse using API that has a link to BibleGateway!
 
 function BibleApiRef() {
-	let [fetchedVerse, setFetchedVerse] = useState({});
+	let [fetchedVerse, setFetchedVerse] = useState([]);
 	let [selectedVerse, setSelectedVerse] = useState("John 1:1");
 
 	function grabVerse(event) {
@@ -75,6 +67,12 @@ function BibleApiRef() {
 					setFetchedVerse(data);
 				});
 			verseInput.value = "";
+		} else if (event.target.id == "verse-random") {
+			fetch(`https://bible-api.com?random=verse`)
+				.then((response) => response.json())
+				.then((data) => {
+					setFetchedVerse(data);
+				});
 		}
 	}
 
@@ -94,22 +92,29 @@ function BibleApiRef() {
 					Powered by Tim Morgan's <a href="https://bible-api.com">Bible API</a>
 				</sub>
 				<h2 className="move-up">{fetchedVerse.reference}</h2>
-				<div className="bible-verse">{fetchedVerse.text}</div>
-				<a
-					target="_blank"
-					href={`https://biblegateway.com/passage/?search=${fetchedVerse.reference}`}>
+				<div className="bible-verse">
+					{fetchedVerse.verses
+						? fetchedVerse.verses.map((verse, i) => (
+								<React.Fragment key={i}>
+									<b className="verse-number">{verse.verse}</b>
+									<span>{verse.text}</span>
+								</React.Fragment>
+						  ))
+						: null}
+				</div>
+				<a target="_blank" href={`https://biblegateway.com/passage/?search=${fetchedVerse.reference}`}>
 					{fetchedVerse.reference} on BibleGateway
 				</a>
 			</div>
 			<div className="d-flex flex-row justify-content-center">
 				<input id="bible-verse-selection" type="text" onKeyDown={grabVerse} />
 			</div>
-			<div className="d-flex flex-row justify-content-center">
-				<button
-					id="verse-select"
-					className="bible-verse-select-button"
-					onClick={grabVerse}>
+			<div id="bible-buttons" className="d-flex flex-row justify-content-evenly">
+				<button id="verse-select" className="bible-verse-select-button" onClick={grabVerse}>
 					Go to Verse
+				</button>
+				<button id="verse-random" className="bible-verse-select-button" onClick={grabVerse}>
+					Get a Random Verse
 				</button>
 			</div>
 		</>
